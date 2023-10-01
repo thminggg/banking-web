1243fb277d9e38a82979ec53fb80f849adb351b3
thminggg <tsehongming21@gmail.com>
2023-10-01 16:28:17 -0700

    feat: update nextjs config

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const nextConfig = {
   basePath: "/banking-web",
+  output: "export",
 };
 module.exports = withBundleAnalyzer(nextConfig);
