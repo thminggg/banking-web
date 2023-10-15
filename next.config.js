2b730a38b62c9d0092b4857bcc2fff1b4bfe511d
thminggg <tsehongming21@gmail.com>
2023-10-15 00:36:37 -0700

    fix: remove next-pwa
    next-pwa is not actively maintained and better handle
    cache and event with custom service worker

--- next.config.js
+++ next.config.js
@@ -2,15 +2,8 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-});
-
 const nextConfig = {
   basePath: "/banking-web",
   reactStrictMode: true,
 };
-module.exports = withBundleAnalyzer(withPWA(nextConfig));
+module.exports = withBundleAnalyzer(nextConfig);
