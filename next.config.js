851ff6cae6a1e0c5d3e5f254a7ac219b4219386c
thminggg <tsehongming21@gmail.com>
2024-10-06 21:42:50 -0700

    chore: update nextjs config as per change of repository name

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 const nextConfig = {
-  basePath: "/banking-web",
+  basePath: "/react-frontend",
   reactStrictMode: true,
 };
 module.exports = withBundleAnalyzer(nextConfig);
